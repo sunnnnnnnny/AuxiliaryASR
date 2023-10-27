139a4fa52fcb08a6746b8d47afe32c017e992240
zhangsan <zhangsan@zhangsandeMacBook-Pro.local>
2023-10-27 19:35:55 +0800

    fix label error

--- prepare/gen_train_val_list.py
+++ prepare/gen_train_val_list.py
@@ -14,15 +14,33 @@ wav_dir = "/data8/master_dataset_model_zhangyuqiang/aishell3_24k_without_normal"
 wav_path_list_tmp = glob(wav_dir + "/*/*.wav")
 print(len(wav_path_list_tmp))
 
+filename2piny = {}
+with open("/data8/master_dataset_model_zhangyuqiang/aishell3_44k/train/content.txt", "r") as log:
+    lines = log.readlines()
+    for line in lines:
+        filename, sentence = line.strip().split(" ", maxsplit=1)
+        piny = " ".join(sentence.split(" ")[1::2])
+        filename2piny[filename] = piny
+with open("/data8/master_dataset_model_zhangyuqiang/aishell3_44k/test/content.txt", "r") as log:
+    lines = log.readlines()
+    for line in lines:
+        filename, sentence = line.strip().split(" ", maxsplit=1)
+        piny = " ".join(sentence.split(" ")[1::2])
+        filename2piny[filename] = piny
+
+
+
 wav_path_list = []
 wav_label_list = []
 
 for wav_path in wav_path_list_tmp:
     txt_path = wav_path.replace(".wav", ".txt")
     if os.path.exists(txt_path):
-        wav_path_list.append(wav_path)
-        with open(txt_path, "r") as log:
-            label = log.readlines()[0].strip()
+        f = open(txt_path, "r")
+        lines = f.readlines()
+        if len(lines) > 0:
+            wav_path_list.append(wav_path)
+            label = lines[0].strip()
             wav_label_list.append(label)
 
 assert len(wav_label_list) == len(wav_path_list)
@@ -35,14 +53,19 @@ assert len(eval_path_list) == len(eval_label_list)
 with open(os.path.join(data_dir, "train_list_aishell3.txt"), "w") as train_log:
     for idx in range(len(train_path_list)):
         path = train_path_list[idx]
-        label = train_label_list[idx]
+        filename = path.split("/")[-1]
+        if filename not in filename2piny:
+            continue
+        label = filename2piny[filename]
         line = path + "|" + label + "|something\n"
         train_log.write(line)
 
 with open(os.path.join(data_dir, "eval_list_aishell3.txt"), "w") as eval_log:
     for idx in range(len(eval_path_list)):
         path = eval_path_list[idx]
+        filename = path.split("/")[-1]
+        if filename not in filename2piny:
+            continue
         label = eval_label_list[idx]
         line = path + "|" + label + "|something\n"
-        train_log.write(line)
-
+        eval_log.write(line)
